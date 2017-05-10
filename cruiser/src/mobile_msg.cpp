c788f891e06b2fb2831adab4ceadd879304148fb
cuijie12358 <Cuijie12358@outlook.com>
2017-05-10 16:28:43 +0800

    v1.9

--- cruiser/src/mobile_msg.cpp
+++ cruiser/src/mobile_msg.cpp
@@ -8,9 +8,13 @@
 #include <dji_sdk/TransparentTransmissionData.h>
 #include <cruiser/Flag.h>
 #include <cruiser/DeltaPosition.h>
+#include <cruiser/TrackingPosition.h>
 
 void GetMobileMsgCallback(const dji_sdk::TransparentTransmissionData& mobileData);
+void Visual_Landing_Cmd(char* mobile_msg,ros::Publisher pub_landing_flag);
+void Object_Tracking_Cmd(char* mobile_msg,ros::Publisher pub_tracking_flag,ros::Publisher pub_tracking_position);
 
+char mobile_msg[10] = {0};
 
 int main(int argc,char **argv)
 {
@@ -18,16 +22,92 @@ int main(int argc,char **argv)
 	ros::NodeHandle nh;
 
 	ros::Subscriber MobileMsg = nh.subscribe("/dji_sdk/data_received_from_remote_device",1,&GetMobileMsgCallback);
-
+	ros::Publisher pub_landing_flag = nh.advertise<cruiser::Flag>("cruiser/landing_flag",1);
+	ros::Publisher pub_tracking_flag = nh.advertise<cruiser::Flag>("cruiser/tracking_flag",1);
+	ros::Publisher pub_tracking_position = nh.advertise<cruiser::TrackingPosition>("cruiser/tracking_position",1);
+	while(ros::ok())
+	{
+		switch (mobile_msg[0])
+		{
+		case 0x01:
+			Visual_Landing_Cmd(mobile_msg,pub_landing_flag);
+			break;
+		case 0x02:
+			Object_Tracking_Cmd(mobile_msg,pub_tracking_flag,pub_tracking_position);
+			break;
+		default:break;
+		}
+		memset(mobile_msg, 0, sizeof(mobile_msg));
+		ros::spinOnce();
+	}
 
 }
 
 void GetMobileMsgCallback(const dji_sdk::TransparentTransmissionData& mobileData)
 {
 	int i=0;
-	while(mobileData.data[i]!= 0 )
+	for(i = 0;i < 10;i++)
 	{
-		ROS_INFO_STREAM(mobileData.data[i++]);
+		mobile_msg[i] = mobileData.data[i];
 	}
+}
 
+void Visual_Landing_Cmd(char* mobile_msg,ros::Publisher pub_landing_flag)
+{
+	switch(mobile_msg[1])
+	{
+	case 0x01:
+	{
+		cruiser::Flag landing_flag;
+		landing_flag.flag = true;
+		pub_landing_flag.publish(landing_flag);
+		ROS_INFO("0101");
+	}
+	break;
+	case 0x03:
+	{
+		cruiser::Flag landing_flag;
+		landing_flag.flag = false;
+		pub_landing_flag.publish(landing_flag);
+		ROS_INFO("0103");
+	}
+	break;
+	default:break;
+	}
+}
+
+void Object_Tracking_Cmd(char* mobile_msg,ros::Publisher pub_tracking_flag,ros::Publisher pub_tracking_position)
+{
+	switch(mobile_msg[1])
+	{
+	case 0x01:
+	{
+		cruiser::Flag tracking_flag;
+		tracking_flag.flag = true;
+		pub_tracking_flag.publish(tracking_flag);
+		ROS_INFO("0201");
+	}
+	break;
+	case 0x03:
+	{
+		cruiser::Flag tracking_flag;
+		tracking_flag.flag = false;
+		pub_tracking_flag.publish(tracking_flag);
+		ROS_INFO("0203");
+	}
+	break;
+	case 0x11:
+	{
+		cruiser::TrackingPosition TrackingPosition;
+		TrackingPosition.a_width_percent = float(mobile_msg[2])/100;
+		TrackingPosition.a_height_percent = float(mobile_msg[3])/100;
+		TrackingPosition.b_width_percent = float(mobile_msg[4])/100;
+		TrackingPosition.b_height_percent = float(mobile_msg[5])/100;
+		pub_tracking_position.publish(TrackingPosition);
+		ROS_INFO_STREAM("a:( "<<TrackingPosition.a_width_percent<<" , "<<TrackingPosition.a_height_percent
+				<<" ) b:( "<<TrackingPosition.b_width_percent<<" , "<<TrackingPosition.b_height_percent);
+	}
+	break;
+	default:break;
+	}
 }
--- cruiser_landing/src/move.cpp
+++ cruiser_landing/src/move.cpp
@@ -9,7 +9,6 @@
 #include "dji_sdk_lib/DJI_API.h"
 #include "dji_sdk_lib/DJICommonType.h"
 #include <dji_sdk/dji_sdk_node.h>
-//#include <dji_sdk/TransparentTransmissionData.h>
 #include <dji_sdk/LocalPosition.h>
 #include <cruiser/DeltaPosition.h>
 #include <cruiser/Flag.h>
@@ -17,80 +16,43 @@
 using namespace DJI;
 using namespace DJI::onboardSDK;
 
-
-void ObtainControlMobileCallback(DJIDrone *drone);
-void ReleaseControlMobileCallback(DJIDrone *drone);
-void TakeOffMobileCallback(DJIDrone *drone);
-void LandingMobileCallback(DJIDrone *drone);
-void GetSDKVersionMobileCallback(DJIDrone *drone);
-void ArmMobileCallback(DJIDrone *drone);
-void DisarmMobileCallback(DJIDrone *drone);
-void GoHomeMobileCallback(DJIDrone *drone);
-void TakePhotoMobileCallback(DJIDrone *drone);
-void StartVideoMobileCallback(DJIDrone *drone);
-void StopVideoMobileCallback(DJIDrone *drone);
-
-
 void LocalPositionCallback(const dji_sdk::LocalPosition& LocalPosition);
-
-void MissionFlagCallback(const cruiser::Flag& landingFlag);
 void DeltaMsgCallback(const cruiser::DeltaPosition& new_location);
+
 DJIDrone *drone;
 DJI::onboardSDK::ROSAdapter *rosAdapter;
 
-long int count = 0;
 int alti_flag = 0;
 int delta_pos = 0;
 float Height;
-bool Mission_flag = 0;
+
 
 int main(int argc,char **argv)
 {
 	ros::init(argc,argv,"landing_move_node");
 	ros::NodeHandle nh;
 
-    ros::Subscriber DeltaMsg = nh.subscribe("cruiser/landing_flag",1,&DeltaMsgCallback);
+	drone = new DJIDrone(nh);
+//	rosAdapter = new DJI::onboardSDK::ROSAdapter;
 
-    if(Mission_flag)
-    {
-    	drone = new DJIDrone(nh);
-    	rosAdapter = new DJI::onboardSDK::ROSAdapter;
+	if(!drone->request_sdk_permission_control())
+		ROS_INFO("Get permission control Error!");
+	ROS_INFO("A");
 
-    	if(!drone->request_sdk_permission_control())
-    		ROS_INFO("Get permission control Error!");
-    	ROS_INFO("A");
+//	uint8_t testdata = 97;
+//	uint8_t len = sizeof(testdata);
+//	rosAdapter->init("/dev/ttyUSB0",230400);//can not locate it before request_permission_control
+//	rosAdapter->sendToMobile(&testdata,len);
 
-    	/*Test part*/
-    	if(!drone->takeoff())
-    		ROS_INFO("Take off Error!");
-    	ROS_INFO("B");
+	ROS_INFO("D");
 
-    	for(int i = 0;i < 40; i++)
-    	{
-    		drone->attitude_control(0x40,0,0,3,0);
-    		usleep(20000);
-    		ROS_INFO("C");
-    	}
-
-    	/*Test part end*/
-
-    	uint8_t testdata = 97;
-    	uint8_t len = sizeof(testdata);
-    	rosAdapter->init("/dev/ttyUSB0",230400);//can not locate it before request_permission_control
-    	rosAdapter->sendToMobile(&testdata,len);
-
-    	ROS_INFO("D");
-
-    	ros::Subscriber Height = nh.subscribe("/dji_sdk/local_position",1,&LocalPositionCallback);
-        ros::Subscriber DeltaMsg = nh.subscribe("cruiser/landing_move",1,&DeltaMsgCallback);
-    }
-
+	ros::Subscriber Height = nh.subscribe("/dji_sdk/local_position",1,&LocalPositionCallback);
+	ros::Subscriber DeltaMsg = nh.subscribe("cruiser/landing_move",1,&DeltaMsgCallback);
 
     ros::Rate rate(10);
-
     while(ros::ok())
     {
-      	if((alti_flag||delta_pos)&&Mission_flag)
+      	if(alti_flag||delta_pos)
     	{
     		drone->landing();
     		drone->release_sdk_permission_control();
@@ -117,10 +79,9 @@ void DeltaMsgCallback(const cruiser::DeltaPosition& new_location)
 		ROS_INFO_STREAM(std::setprecision(2) << std::fixed
 				<< "position = (" << new_location.delta_X << "," << new_location.delta_Y << ")");
 
+		if((new_location.delta_X < 0.2) && (new_location.delta_Y < 0.2)&&(Height < 1.9))
+			delta_pos = 1;
 	}
-	if((new_location.delta_X < 0.2) && (new_location.delta_Y < 0.2)&&(Height < 1.9))
-		delta_pos = 1;
-
 /*
 	float Velociy_X_max = new_location.delta_X * 2;
 	float Velociy_Y_max = new_location.delta_Y * 2;
@@ -166,7 +127,7 @@ void LocalPositionCallback(const dji_sdk::LocalPosition& LocalPosition)
 	if(Height > 0.8)
 	{
 		alti_flag = 0;
-		for(int i = 0;i < 10; i++)
+		for(int i = 0;i < 20; i++)
 		{
 			drone->attitude_control(0x40,0,0,-1,0);//水平速度
 			usleep(20000);
@@ -179,70 +140,4 @@ void LocalPositionCallback(const dji_sdk::LocalPosition& LocalPosition)
 
 
 
-void MissionFlagCallback(const cruiser::Flag& landingFlag)
-{
-	Mission_flag = landingFlag.flag;
-}
 
-
-
-
-
-
-
-
-//! Callback functions for Mobile Commands
-void ObtainControlMobileCallback(DJIDrone *drone)
-{
-  drone->request_sdk_permission_control();
-}
-
-void ReleaseControlMobileCallback(DJIDrone *drone)
-{
-  drone->release_sdk_permission_control();
-}
-
-void TakeOffMobileCallback(DJIDrone *drone)
-{
-  drone->takeoff();
-}
-
-void LandingMobileCallback(DJIDrone *drone)
-{
-  drone->landing();
-}
-
-void GetSDKVersionMobileCallback(DJIDrone *drone)
-{
-  drone->check_version();
-}
-
-void ArmMobileCallback(DJIDrone *drone)
-{
-  drone->drone_arm();
-}
-
-void DisarmMobileCallback(DJIDrone *drone)
-{
-  drone->drone_disarm();
-}
-
-void GoHomeMobileCallback(DJIDrone *drone)
-{
-  drone->gohome();
-}
-
-void TakePhotoMobileCallback(DJIDrone *drone)
-{
-  drone->take_picture();
-}
-
-void StartVideoMobileCallback(DJIDrone *drone)
-{
-  drone->start_video();
-}
-
-void StopVideoMobileCallback(DJIDrone *drone)
-{
-  drone->stop_video();
-}
