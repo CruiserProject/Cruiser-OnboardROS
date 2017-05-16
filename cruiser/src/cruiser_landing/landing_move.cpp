ae74e6c4e3196512dc22f23945968f821c33d734
cuijie12358 <Cuijie12358@outlook.com>
2017-05-16 23:11:31 +0800

    v3.0.2 build class LandingMove

--- cruiser/src/cruiser_landing/landing_move.cpp
+++ cruiser/src/cruiser_landing/landing_move.cpp
@@ -7,38 +7,131 @@
 DJIDrone *drone;
 CruiserDrone* cruiserdrone;
 
-bool alti_flag = false;
-bool delta_pos = false;
-float Height;
-float Height_Last;
+class LandingMove
+{
+private:
+	ros::Subscriber DeltaMsg;
+	ros::Subscriber Height;
+	bool height_change_flag = false;
+	float local_height = 0.0;
+	float height_last = 0.0;
+
+public:
+	LandingMove(ros::NodeHandle& nh)
+	{
+		ros::Subscriber DeltaMsg = nh.subscribe("cruiser/landing_move",1,&LandingMove::DeltaMsgCallback,this);
+		ros::Subscriber Height = nh.subscribe("/dji_sdk/local_position",1,&LandingMove::AutoHeightChanged,this);
+	}
+
+	~LandingMove()
+	{
+	//	cv::destroyWindow(OPENCV_WINDOW);
+	}
+	bool alti_flag = false;
+	bool delta_pos = false;
+
+	void DeltaMsgCallback(const cruiser::DeltaPosition& new_location)
+	{
+		this->height_change_flag = new_location.state;
+		if(drone->gimbal_angle_control(0, -900, 0, 10))
+		{
+			ROS_INFO_STREAM("landing_move_node : gimbal angle changed.");
+			usleep(100000);
+		}
+
+		if (new_location.state)
+		{
+			drone->attitude_control(0x81,new_location.delta_X_meter,new_location.delta_Y_meter,0,0);//location
+			usleep(20000);
+		}
+	//	float Height = 0;
+	//	if(drone->gimbal_angle_control(0, -900, 0, 10))
+	//		ROS_INFO_STREAM("landing_move_node : gimbal angle changed.");
+	//	usleep(100000);
+	//	if (new_location.state)
+	//	{
+	//		float Velocity_X = new_location.delta_X_meter;
+	//		float Velocity_Y = new_location.delta_Y_meter;
+	//
+	//		for(int i = 0;i < 50; i++)
+	//		{
+	//			drone->attitude_control(0x40,Velocity_X,Velocity_Y,0,0);//水平速度
+	//			usleep(20000);
+	//		}
+	//		Height = cruiserdrone->GetHeightNow();
+	//		if(Height > 1)
+	//		{
+	//			for(int i = 0;i < 20; i++)
+	//			{
+	//				drone->attitude_control(0x40,0,0,-1,0);//水平速度
+	//				usleep(20000);
+	//			}
+	//		}
+	//		else
+	//		{
+	//			if(abs(Height_Last-Height) < 1)	alti_flag = true;
+	//		}
+	//
+	//		ROS_INFO_STREAM(std::setprecision(2) << std::fixed
+	//				<< "landing_move_node : move position = (" << new_location.delta_X_meter << ","
+	//				<< new_location.delta_Y_meter << ")");
+	//
+	//		if((new_location.delta_X_meter < 0.2) && (new_location.delta_Y_meter < 0.2)&&(Height < 1.9))
+	//			delta_pos = true;
+	//	}
+	}
+
+	void AutoHeightChanged(const dji_sdk::LocalPosition& new_height)
+	{
+		this->height_last = this->local_height;
+		this->local_height = cruiserdrone->GetHeightNow();
+		if(this->local_height > 1)
+		{
+			drone->attitude_control(0x81,0,0,0.2,0);//location
+			usleep(30000);
+		}
+		else
+		{
+			if(abs(this->height_last - this->local_height) < 1)	this->alti_flag = true;
+		}
+	}
+
+	void SetAltiFlag(bool flag)
+	{
+		this->alti_flag = flag;
+	}
+	void SetDeltaPos(bool flag)
+	{
+		this->delta_pos = flag;
+	}
+
+};
 
 
-void DeltaMsgCallback(const cruiser::DeltaPosition& new_location);
+
+
+
 
 int main(int argc,char **argv)
 {
 	ros::init(argc,argv,"landing_move_node");
 	ros::NodeHandle nh;
-
-	cruiserdrone = new CruiserDrone(nh);
-	drone = new DJIDrone(nh);
-
-	ros::Subscriber DeltaMsg = nh.subscribe("cruiser/landing_move",1,&DeltaMsgCallback);
-
-	ros::Rate rate(0.5);
-
 	if(drone->request_sdk_permission_control())
 		ROS_INFO_STREAM("landing_move_node : initialization and get control.");
 
+	ros::Rate rate(0.5);
+	LandingMove* landing_move_node = new LandingMove(nh);
+	drone = new DJIDrone(nh);
+	cruiserdrone = new CruiserDrone(nh);
     while(ros::ok())
     {
-      	if(alti_flag||delta_pos)
+      	if(landing_move_node->alti_flag||landing_move_node->delta_pos)
     	{
       		cruiserdrone->SendVtlLandingMsg();
     		drone->landing();
     		cruiserdrone->SendSucLandingMsg();
-    		alti_flag = false;
-    		delta_pos = false;
+    		landing_move_node->SetAltiFlag(false);
+    		landing_move_node->SetDeltaPos(false);
     		if(drone->gimbal_angle_control(0, 0, 0, 10))
     			ROS_INFO_STREAM("landing_move_node : gimbal angle changed.");
     		usleep(100000);
@@ -47,71 +140,3 @@ int main(int argc,char **argv)
         ros::spinOnce();
     }
 }
-
-void DeltaMsgCallback(const cruiser::DeltaPosition& new_location)
-{
-	float Height = 0;
-	if(drone->gimbal_angle_control(0, -900, 0, 10))
-		ROS_INFO_STREAM("landing_move_node : gimbal angle changed.");
-	usleep(100000);
-	if (new_location.state)
-	{
-		drone->attitude_control(0x81,new_location.delta_X_meter,new_location.delta_Y_meter,0,0);//location
-		usleep(20000);
-		Height = cruiserdrone->GetHeightNow();
-		if(Height > 1)
-		{
-			drone->attitude_control(0x81,0,0,0.2,0);//location
-			usleep(20000);
-		}
-		else
-		{
-			if(abs(Height_Last-Height) < 1)	alti_flag = true;
-		}
-
-		ROS_INFO_STREAM(std::setprecision(2) << std::fixed
-				<< "landing_move_node : move position = (" << new_location.delta_X_meter << ","
-				<< new_location.delta_Y_meter << ")");
-
-		if((new_location.delta_X_meter < 0.2) && (new_location.delta_Y_meter < 0.2)&&(Height < 1.9))
-			delta_pos = true;
-	}
-//	float Height = 0;
-//	if(drone->gimbal_angle_control(0, -900, 0, 10))
-//		ROS_INFO_STREAM("landing_move_node : gimbal angle changed.");
-//	usleep(100000);
-//	if (new_location.state)
-//	{
-//		float Velocity_X = new_location.delta_X_meter;
-//		float Velocity_Y = new_location.delta_Y_meter;
-//
-//		for(int i = 0;i < 50; i++)
-//		{
-//			drone->attitude_control(0x40,Velocity_X,Velocity_Y,0,0);//水平速度
-//			usleep(20000);
-//		}
-//		Height = cruiserdrone->GetHeightNow();
-//		if(Height > 1)
-//		{
-//			for(int i = 0;i < 20; i++)
-//			{
-//				drone->attitude_control(0x40,0,0,-1,0);//水平速度
-//				usleep(20000);
-//			}
-//		}
-//		else
-//		{
-//			if(abs(Height_Last-Height) < 1)	alti_flag = true;
-//		}
-//
-//		ROS_INFO_STREAM(std::setprecision(2) << std::fixed
-//				<< "landing_move_node : move position = (" << new_location.delta_X_meter << ","
-//				<< new_location.delta_Y_meter << ")");
-//
-//		if((new_location.delta_X_meter < 0.2) && (new_location.delta_Y_meter < 0.2)&&(Height < 1.9))
-//			delta_pos = true;
-//	}
-
-}
-
-
